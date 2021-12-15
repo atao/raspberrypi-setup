a664593540214b283c56e6dd18f90e9598ab5799
ATAO <atao@users.noreply.github.com>
2021-12-15 12:03:41 +0100

    Add colors

--- install.sh
+++ install.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
-echo "======================================================="
-echo "===          Setting up your Raspberry Pi           ==="
-echo "======================================================="
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
+reset=$(tput sgr0)
+
+rouge=$(	tput setaf 1)
+vert=$(		tput setaf 2)
+jaune=$(	tput setaf 3)
+bleu=$(		tput setaf 4)
+violet=$(	tput setaf 5)
+cyan=$(		tput setaf 6)
+gris=$(		tput setaf 7)
+
+# Fonctions
+shw_norm () { echo -en "${bold}$(tput setaf 9)${@}${reset}";	}
+shw_info () { echo -en "${bold}${cyan}${@}${reset}";		}
+shw_OK ()   { echo -en "${bold}${vert}OK!${@}${reset}";		}
+shw_warn () { echo -en "${bold}${violet}${@}${reset}";		}
+shw_err ()  { echo -en "${bold}${rouge}${@}${reset}";		}
+gris() 	    { echo -en "${bold}${gris}${@}${reset}";		}
+header()    { echo -e "${bold}${jaune}$*${reset}";		}
+headerU()   { echo -e "${underline}${bold}${jaune}$*${reset}";  }
+
+clear && echo -e "\n\n"
+header "======================================================="
+header "===          Setting up your Raspberry Pi           ==="
+headerU "======================================================="
 
 if [ $# -eq 0 ]
   then
-    echo "No arguments supplied, please choose the roles to install"
-    echo "exiting..."
+    shw_warn "No arguments supplied, please choose the roles to install"
+    shw_warn "exiting..."
     exit 1
 fi
 
@@ -20,18 +43,18 @@ mkdir $installdir
 
 git clone https://github.com/atao/raspberrypi-setup.git $installdir
 if [ ! -d $installdir ]; then
-    echo "git cloned failed"
-    echo "exiting..."
+    shw_err "git cloned failed"
+    shw_err "exiting..."
     exit 1
 else
     cd $installdir
     ansible-playbook playbook.yml -i localhost --tags $1
 fi
 
-echo "cleaning up..."
+shw_info "cleaning up..."
 
 rm -Rfv /tmp/$installdir
 
-echo "and we are done! Enjoy!"
+shw_OK "and we are done! Enjoy!"
 
 exit 0
