5cbefd17f184e9a66a2fed11372d0a00a5fb25f6
ATAO <atao@users.noreply.github.com>
2022-09-09 17:11:52 +0200

    Update check_multitor
    Add checks and errors catching

--- scripts/check_multitor.py
+++ scripts/check_multitor.py
@@ -5,13 +5,22 @@ import requests
 from lxml import html
 
 proxies = {"http": "socks5://localhost:9000", "https": "socks5://localhost:9000"}
-# proxies = set()
 
 r = requests.Session()
 r.proxies.update(proxies)
-response = r.get("https://check.torproject.org", proxies=proxies)
-tree = html.fromstring(response.text)
-data = tree.xpath('//div[@class="content"]')
-for p in data:
-    print(p.xpath('.//h1/text()')[0].strip())
-    print(p.xpath('.//p/text()')[0],p.xpath('.//p/strong/text()')[0])
+try:
+    response = r.get("https://check.torproject.org", proxies=proxies)
+except Exception as e:
+    print(e)
+    exit(2)
+
+if response.status_code == 200:
+    tree = html.fromstring(response.text)
+    data = tree.xpath('//div[@class="content"]')
+    for p in data:
+        print(p.xpath('.//h1/text()')[0].strip())
+        print(p.xpath('.//p/text()')[0],p.xpath('.//p/strong/text()')[0])
+else:
+    print("Request error")
+    exit(2)
+    
